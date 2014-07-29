f1024fbab9123ba9f17f60d9749424575d16afd4
bruno <bruno@bruno-PC>
2014-07-29 15:00:44 -0300

    Commit inicial do projeto

--- Exemplo/src/exemplo/Exemplo.java
+++ Exemplo/src/exemplo/Exemplo.java
@@ -14,6 +14,6 @@ public class Exemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("TESTE");
     }
 }
