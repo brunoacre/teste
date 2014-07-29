15d7f8421ab27d44e69a48efbb3b0bc594c6b2d0
bruno <bruno@bruno-PC>
2014-07-29 16:53:38 -0300

    teste

--- Exemplo/src/exemplo/Exemplo.java
+++ Exemplo/src/exemplo/Exemplo.java
@@ -14,6 +14,7 @@ public class Exemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("TESTE");
+        System.out.println("TESTE");System.out.println("TESTE");
+        
     }
 }
